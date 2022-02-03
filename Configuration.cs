c39941e3ff84f9cadcec78f857e0edacafbf5eef
Bradley O'Connell <bradleyocon@gmail.com>
2022-02-03 16:18:52 -0500

    Created Configuration reader

---
+++ Configuration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DiscordBoostRoleBot
+{
+    internal class Configuration
+    {
+        public string Token { get; set; } = null!;
+
+        public static Configuration ReadConfig(string configPath = "config.json")
+        {
+            string jsonString = File.ReadAllText(configPath);
+            JsonSerializerOptions? options = new()
+            {
+                AllowTrailingCommas = true,
+                PropertyNameCaseInsensitive = true,
+                ReadCommentHandling = JsonCommentHandling.Skip,
+                WriteIndented = true
+            };
+            Configuration configuration = JsonSerializer.Deserialize<Configuration>(jsonString, options) ?? throw new InvalidOperationException("Configuration cannot be null");
+            return configuration;
+        }
+    }
+}
